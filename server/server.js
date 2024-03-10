b9a1d6ad9cab282755f9e16a687002d6f16e8ca9
Elgin Li <elginli@Elgins-MacBook-Air.local>
2024-03-09 16:49:57 -0800

    Logout/Login/Profile

--- server/server.js
+++ server/server.js
@@ -5,3 +5,25 @@ app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
 })
 app.listen(80, () => {console.log("Server started on port 80")})
+
+
+//testing
+const { auth } = require('express-openid-connect');
+console.log(auth)
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: '-RThkMAAJy31ASdb0FgVHGfrQNcKD1NcxvzD0K9bQZxxw5bgOkXRc2oC5vG4ek3Mv',
+  baseURL: 'http://localhost:3000',
+  clientID: '18pwbDRgJaFWrqqyv7TbzOvSBkFlZ1zx',
+  issuerBaseURL: 'https://dev-xo2t3urnf411ozm0.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
